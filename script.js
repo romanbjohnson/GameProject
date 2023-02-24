b7cfe6f6e0191253f182e431a5dfe7c7ac31ad6f
romanbjohnson <106290601+romanbjohnson@users.noreply.github.com>
2023-02-24 02:32:21 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
- //const body = document.body 
- // body.append("Hello world", "Bye")
-// const div= document.createElement('div')
+const body = document.body 
+ const div = document.querySelector('div')
+ const spanHi = document.querySelector('#hi')
+ const spanBye = document.querySelector('#bye')
+ // console.log(spanHi.getAttribute('id'))
+ console.log(spanHi.setAttribute("id", "sdfsdfd"))
+ console.log(spanHi.title)
+ //shortcuts for Dom Manipulation
+ //spanBye.remove()
+ //div.removeChild(spanHi)
+ //spanHi.remove()
+
+// const div = document.createElement("div")
+// const strong = document.createElement('strong')
+// strong.innerText = "Hello World 2"
 
 // div.innerText = "Hello World"
-// div.textContent= "Hello World 2"
+// div.textContent = "<strong>Hello World 2<strong>"
+// div.innerHTML = "<strong>Hello World 2<strong>"
+
+// div.append(strong)
 // body.append(div)
+
+
+ //const = document.querySelector('div')
+ //console.log(div.textContent)
+ //console.log(div.innerText)
 
 
