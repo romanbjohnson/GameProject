68c6fbd81f7c544f3ce4141d6b7aca2b3ad7b20d
romanbjohnson <106290601+romanbjohnson@users.noreply.github.com>
2023-02-24 00:12:25 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+ //const body = document.body 
+ // body.append("Hello world", "Bye")
+// const div= document.createElement('div')
+
+// div.innerText = "Hello World"
+// div.textContent= "Hello World 2"
+// body.append(div)
+
+
