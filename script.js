a753b291eaac6e0ffe321cfc06c750e1a64b07e4
romanbjohnson <106290601+romanbjohnson@users.noreply.github.com>
2023-02-27 02:48:49 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,48 +1,32 @@
-const body = document.body 
- const div = document.querySelector('div')
- const spanHi = document.querySelector('#hi')
- const spanBye = document.querySelector('#bye')
- // console.log(spanHi.getAttribute('id'))
- console.log(spanHi.setAttribute("id", "sdfsdfd"))
- //spanHi.removeAttribute("title")
- // spanHi.removeAttribute("id")
- console.log(spanHi.dataset.longerName)
- console.log(spanHi.title)
- // spanHi.style.color = "red"
- //spanHi.style.backgroundColor = "red"
+const game = document.getElementById ('game')
+const scoreDisplay = document.getElementById ('score')
 
- //spanHi.dataset.newName = "hi"
- //spanHi.classList
- // spanHi.classList.toggle("hi 2")
- // spanHi.classList.toggle("hi 2", false)
- // spanHi.classList.toggle("hi 2", true)
- //shortcuts for Dom Manipulation
- //spanBye.remove()
- //div.removeChild(spanHi)
- //spanHi.remove()
+const jeopardyCategories = [
+    {
+        genre: "WHO",
+        questions: []
+    },
+    {
+        genre: "WHERE",
+        questions: []  
 
-// const div = document.createElement("div")
-// const strong = document.createElement('strong')
-// strong.innerText = "Hello World 2"
+    },
 
-// div.innerText = "Hello World"
-// div.textContent = "<strong>Hello World 2<strong>"
-// div.innerHTML = "<strong>Hello World 2<strong>"
+    {
+        genre: "WHAT",
+        questions: []  
 
-// div.append(strong)
-// body.append(div)
+   },
+   {
+         genre: "WHEN",
+         questions: []  
+
+  },
+  {
+         genre: "WHY",
+         questions: []  
+
+},
 
 
- //const = document.querySelector('div')
- //console.log(div.textContent)
- //console.log(div.innerText)
-
-
- /*
-The code below will change
-the heading with id = "myH"
-and the paragraph with id = "myP"
-in my web page:
-
-
-*/
+]
