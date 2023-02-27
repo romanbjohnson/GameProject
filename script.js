e45757525467d71ce1d7d0d1f48ae68513b8e821
romanbjohnson <106290601+romanbjohnson@users.noreply.github.com>
2023-02-27 15:08:13 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,100 @@
 const game = document.getElementById ('game')
 const scoreDisplay = document.getElementById ('score')
 
+
 const jeopardyCategories = [
     {
         genre: "WHO",
-        questions: []
+        questions: [
+            
+            { question: "Who was the first American in space?",
+              answers: "["Buzz Aldrin", "Mae Jemison"]",
+              correct: "",
+              level: "easy"
+
+
+                
+           
+        //need to add toLowercase function for the answer input so that any answer that is correct will result
+
+             },
+
+       ]
+
     },
     {
         genre: "WHERE",
-        questions: []  
+        questions: [
 
-    },
+          {  question: "Where is the world did the first person in the world come from to arrive in space?",
+             answers: "["", ""]",
+             correct: "", 
+             level: "moderate"
+
+
+         }, 
+
+        ]  
+         
+      //need to add toLowercase function for the answer input so that any answer that is correct will result
+
+           },
 
     {
         genre: "WHAT",
-        questions: []  
+        questions: [ 
+
+        { question: "What city is the headquarters for NASA located?",
+            answers: 
+            correct: 
+            level: "medium-hard"
+
+    
+
+
+              
+         
+      //need to add toLowercase function for the answer input so that any answer that is correct will result
+
+           },
+
+    
+        ]    
+
 
    },
    {
          genre: "WHEN",
-         questions: []  
+         questions: [ 
+
+         { question: "When did the first person walk on the moon?",
+            answers: 
+            correct: 
+            level: "hard"
+
+
+              
+         
+  
+           },
+
+        ] 
 
   },
   {
          genre: "WHY",
-         questions: []  
+         questions: [  
 
+         { question: "Why was Apollo 11 planned?",
+            answers: "",
+            correct: "",
+            level:  "hardest"
+
+           },
+
+        ]
 },
+
 
 
 ]
